1fee3b28dffba2712288d08783ecbb6559b7e4f3
ivanarganda <32928891+ivanarganda@users.noreply.github.com>
2023-11-30 22:37:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const app = express();
 const server = require('http').createServer(app);
 
+const API_KEY = 'rnd_n8GiDBzS6xnXLrk11SEbQ7l85hrw';
+
 const allowedOrigins = ['https://igvdeveloper.com', 'http://localhost:5173', 'https://igvdeveloper-ws-com.onrender.com'];
 
 const corsOptions = {
@@ -17,18 +19,29 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.get('/download', (req, res) => {
-  res.download(__dirname + '/documents/cv.pdf');
+app.get('/download/:key', (req, res) => {
+
+  let json_message = {'Error':401, 'Msg':'No autorized to download'};
+
+  if ( req.params.key !== API_KEY ){
+
+    res.status(401).json( json_message );
+
+  } else {
+
+    res.download(__dirname + '/documents/cv.pdf');
+
+  }
 });
 
 // Catch-all route handler for unknown paths
 app.all('*', (req, res) => {
-  res.status(404).json({
-    'Error': 404,
-    'Msg': 'Not found services path ' + req.originalUrl,
-  });
+
+  let json_message = { 'Error': 404, 'Msg': 'Not found services path ' + req.originalUrl };
+
+  res.status(404).json(json_message);
+  console.log(json_message);
+
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+server.listen(3000);
