345e14e3dd0912051b89b460a647a3ee23a20ff4
ivanarganda <32928891+ivanarganda@users.noreply.github.com>
2023-12-08 10:04:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ const cors = require('cors');
 const app = express();
 const server = require('http').createServer(app);
 
+const bodyParser = require("body-parser");
+
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ extended: true }));
+
+//Importando la biblioteca nodemailer en tu archivo
+const nodemailer = require("nodemailer"); 
+
+const transporter = nodemailer.createTransport({ 
+  /**
+   * Para utilizar otro servicio de correo electrónico, como Yahoo o Outlook, debes
+   * cambiar el valor de la propiedad service y ajustar la configuración de autenticación correspondiente.
+   */
+  service: "gmail",
+  auth: {
+    user: "ivanartista96@gmail.com",
+    pass: "znuq dwbl wxra ekkg",
+  },
+});
+
 const API_KEY = 'rnd_n8GiDBzS6xnXLrk11SEbQ7l85hrw';
 
 const allowedOrigins = ['https://igvdeveloper.com', 'http://localhost:5173', 'https://igvdeveloper-ws-com.onrender.com'];
@@ -33,6 +53,27 @@ app.get('/download/:key', (req, res) => {
 
   }
 });
+
+app.post('/sendemail',(req,res)=>{
+
+  const mailOptions = {
+    from: req.body.from,
+    to: 'ivanartista96@gmail.com',
+    subject: req.body.subject,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(401).json('Email error');
+      console.log("Error al enviar el correo:", error);
+    } else {
+      res.status(200).json('Email sent ' + req.body );
+      console.log("Correo enviado:", info.response); 
+    } 
+  }); 
+
+}) 
 
 // Catch-all route handler for unknown paths
 app.all('*', (req, res) => {
