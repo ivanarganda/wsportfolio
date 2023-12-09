3ddc46f2f5cb6027e9ae985c6562f36fbe00a0e4
ivanarganda <32928891+ivanarganda@users.noreply.github.com>
2023-12-09 11:52:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,18 +11,6 @@ app.use( bodyParser.urlencoded({ extended: true }));
 //Importando la biblioteca nodemailer en tu archivo
 const nodemailer = require("nodemailer"); 
 
-const transporter = nodemailer.createTransport({ 
-  /**
-   * Para utilizar otro servicio de correo electrónico, como Yahoo o Outlook, debes
-   * cambiar el valor de la propiedad service y ajustar la configuración de autenticación correspondiente.
-   */
-  service: "gmail",
-  auth: {
-    user: "ivanartista96@gmail.com",
-    pass: "znuq dwbl wxra ekkg",
-  },
-});
-
 const API_KEY = 'rnd_n8GiDBzS6xnXLrk11SEbQ7l85hrw';
 
 const allowedOrigins = ['https://igvdeveloper.com', 'http://localhost:5173', 'https://igvdeveloper-ws-com.onrender.com'];
@@ -56,11 +44,23 @@ app.get('/download/:key', (req, res) => {
 
 app.post('/sendemail',(req,res)=>{
 
+  const transporter = nodemailer.createTransport({ 
+  
+    host: 'smtp.gmail.com', 
+    auth: {
+        user: 'ivanartista96@gmail.com', 
+        pass: 'gyjt iljy aqng mowb'   
+    }
+  
+  });  
+
   const mailOptions = {
-    from: req.body.from,
-    to: 'ivanartista96@gmail.com',
-    subject: req.body.subject,
-    text: req.body.message,
+
+    from:req.body.from, 
+    to:'ivanartista96@gmail.com', 
+    subject:`${req.body.fullname} <${req.body.from}> wants to contact on you:"${req.body.subject}"`,
+    html: req.body.message,
+    
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
@@ -68,7 +68,7 @@ app.post('/sendemail',(req,res)=>{
       res.status(401).json('Email error');
       console.log("Error al enviar el correo:", error);
     } else {
-      res.status(200).json('Email sent ' + req.body );
+      res.status(200).json('Email sent ' + JSON.stringify(req.body) ); 
       console.log("Correo enviado:", info.response); 
     } 
   }); 
@@ -81,7 +81,7 @@ app.all('*', (req, res) => {
   let json_message = { 'Error': 404, 'Msg': 'Not found services path ' + req.originalUrl };
 
   res.status(404).json(json_message);
-  console.log(json_message);
+  console.log(json_message); 
 
 });
 
